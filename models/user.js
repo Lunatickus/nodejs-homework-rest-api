c95b770b5a7f7159e1009f5ee5e7bdc71e14d10a
Igor Lunyakin <lunyaigor484@gmail.com>
2023-12-13 16:47:59 +0200

    hw-4

---
+++ models/user.js
@@ -0,0 +1,61 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+
+const { handleMongooseError } = require("../helpers");
+const { subscriptionEnum } = require("../constants");
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: Object.values(subscriptionEnum),
+      default: subscriptionEnum.STARTER,
+    },
+    token: {
+      type: String,
+      default: "",
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const registerSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+  subscription: Joi.string().valid(...Object.values(subscriptionEnum)),
+});
+
+const loginSchema = Joi.object({
+  password: Joi.string().required(),
+  email: Joi.string().required(),
+});
+
+const updateSubscriptionSchema = Joi.object({
+  subscription: Joi.string()
+    .required()
+    .valid(...Object.values(subscriptionEnum)),
+});
+
+const schemas = {
+  registerSchema,
+  loginSchema,
+  updateSubscriptionSchema,
+};
+
+const User = model("user", userSchema);
+
+module.exports = {
+  User,
+  schemas,
+};
